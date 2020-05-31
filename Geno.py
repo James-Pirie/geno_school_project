fb48df91b20b4508e6dda1452f0b263a3ce7c925
James Pirie <131404@cloud.rangitoto.school.nz>
2020-05-31 23:06:56 +1200

    Added comments

--- Geno.py
+++ Geno.py
@@ -1,14 +1,16 @@
 import random
 
 
+# setup the geno object, where a single persons dna will be generated
 class Geno:
-    def __init__(self, genotype_size,):
+    def __init__(self, genotype_size,):  # initialize the object
+        """setup the possible diseases"""
         self.disease_pos_or_neg = {"cancer": False,
                                    "heart_disease": False,
                                    "alzheimer": False,
                                    "cystic_fibrosis": False,
                                    "crohns_disease": False}
-        self.size = genotype_size
+        self.size = genotype_size  # setup the size of the dna
         self.dna = ""
         self.full_genotype = ""
         self.probability = {"cancer": 0,
@@ -18,32 +20,38 @@ class Geno:
                             "crohns_disease": 0}
 
     def get_genotype(self):
+        """generate the dna"""
         base_pairs = ["G", "A",
-                      "C", "T"]
+                      "C", "T"]  # all possible base values
         number_of_base_pairs = len(base_pairs)
-        for i in range(self.size):
+        for i in range(self.size):  # randomly generate dna the length required made of base values
             base_pairs_chance = random.randint(0, number_of_base_pairs - 1)
             self.dna += base_pairs[base_pairs_chance]
 
     def find_disease(self):
+        """Analyse the dna for diseases"""
+        # all disease base patterns
         cancer = "GATTA"
         heart_disease = "CCTGA"
         alzheimer = "GAATT"
         cystic_fibrosis = "CCCAC"
         crohns_disease = "TTACG"
+        # every possible disease
         list_of_diseases = [cancer, heart_disease,
                             alzheimer, cystic_fibrosis,
                             crohns_disease]
         list_of_diseases_names = ["cancer", "heart_disease", "alzheimer",
                                   "cystic_fibrosis", "crohns_disease"]
 
+        # look for every disease type in the dna
         for i in range(len(list_of_diseases)):
             if list_of_diseases[i] in self.dna:
-                self.disease_pos_or_neg[str(list_of_diseases_names[i])] = True
+                self.disease_pos_or_neg[str(list_of_diseases_names[i])] = True  # if disease found return true
                 self.probability[list_of_diseases_names[i]] = self.dna.count(list_of_diseases[i])
 
         print("*******************************************************************************************************")
         for x in range(len(list_of_diseases)):
+            # determine the risk factor based on how many instances of a disease there is in the dna
             risk: str
             risk_for_particular_disease = self.probability[list_of_diseases_names[x]]
 
@@ -57,15 +65,16 @@ class Geno:
                 risk = "High"
             else:
                 risk = "Very High"
-            replica_dna_sample = []
+            replica_dna_sample = []  # duplicate the dna
             previous_values = 0
             location_of_disease = []
             if risk_for_particular_disease > 0:
-                replica_dna_sample = self.dna.split(list_of_diseases[x])
-                for i in range(len(replica_dna_sample)-1):
+                replica_dna_sample = self.dna.split(list_of_diseases[x])  # split the dna where the dna is
+                for i in range(len(replica_dna_sample)-1):  # check the length of the list items to find dna locations
                     location_of_disease.append((len(replica_dna_sample[i])+1)+previous_values)
                     previous_values += (len(replica_dna_sample[i])+1)
 
+            # print out the report
             print(f" disease: {list_of_diseases_names[x]}, \n increased probability: "
                   f"{self.disease_pos_or_neg[list_of_diseases_names[x]]} \n "
                   f"genetic preposition: {self.disease_pos_or_neg[list_of_diseases_names[x]]}  \n"
--- Main.py
+++ Main.py
@@ -3,27 +3,29 @@ import textwrap
 
 
 def main():
+    # ask how many genes the user would like to generate
     number_to_analyse = int(input("How many genotypes would you like to analise?: "))
-
+    # all possible parameters for yes
     yes = ["yes", "ya", "y",
            "yeah", "ye", "affirmative",
            "positive", "true", "ye"]
+    # ask user if they would like to print the dna in full
     print_dna = input("Would you like to print the genotype in full for manual analysis?")
 
+    # for every time the user would like to repeat, repeat
     for i in range(number_to_analyse):
         print("*******************************************************************************************************")
-        print(f"Genotype Data {i}")
-        man = Geno(1000)
-        man.get_genotype()
-        man.find_disease()
-        if print_dna.lower().strip() in yes:
-
+        print(f"Genotype Data {i}")  # print the dna id number
+        man = Geno(1000)  # set the length of the dna
+        man.get_genotype()  # generate dna string
+        man.find_disease()  # search the dna for diseases
+        if print_dna.lower().strip() in yes:  # check if the user wants to print the dna
+            # print the dna
             print(f"genetic code:\n{textwrap.fill(man.dna, 101)}")
-        if i != number_to_analyse - 1:
+        if i != number_to_analyse - 1:  # pause the printing of the report until the user presses enter
             press_enter = input("Press enter to continue")
 
 
+# if the program is run call main function
 if __name__ == '__main__':
-
-    main()
-
+    main()  # activate main function
